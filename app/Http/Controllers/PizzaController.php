7b89f31f62b81be66d195db03923ef5be2255b49
andrew <alm3allmandy@gmail.com>
2021-10-23 11:14:05 +0200

    added a button to complete/delete order

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -47,4 +47,11 @@ class PizzaController extends Controller
 
         return redirect('/')->with('msg', 'Thanks for ur Order');
     }
+
+    public function destroy($id){
+        $pizza = Pizza::findOrFail($id);
+        $pizza->delete();
+
+        return redirect('/pizzas');
+    }
 }
